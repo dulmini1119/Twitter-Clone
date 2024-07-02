caecc3727a3f1f7843d8395d7582e28e1b9bc52a
Dulmini Senewirathna <senewirathnapabodha@gmail.com>
2024-07-02 23:54:30 +0530

    commit

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,3 +1,4 @@
+import { generateTokenAndSetCookie } from '../lib/utils/generateToken.js';
 import User from '../models/user.model.js';
 import bcrypt from 'bcryptjs';
 
@@ -33,7 +34,7 @@ export const signup = async (req,res) => {
         });
 
         if(newUser){
-            generateTokenAndSetCookie(newUser,_id,res)
+            generateTokenAndSetCookie(newUser._id,res)
             await newUser.save();
 
             res.status(201).json({
@@ -44,7 +45,7 @@ export const signup = async (req,res) => {
                 followers:newUser.followers,
                 following:newUser.following,
                 profileImg:newUser.profileImg,
-                coverImg:newUser,coverImg,
+                coverImg:newUser.coverImg,
                 });
             
         }else{
@@ -59,13 +60,51 @@ export const signup = async (req,res) => {
 }
 
 export const login = async (req,res) => {
-    res.json({
-        data: "You hit the login endpoint",
-    });
+    try{
+        const {username,password} = req.body;
+        const user = await User.findOne({username});
+        const isPasswordCorrect = await bcrypt.compare(password, user?.password || "")
+
+        if(!user || !isPasswordCorrect){
+            return res.status(400).json({error: "Invalid credentials."});
+        }
+
+        generateTokenAndSetCookie(user._id,res);
+
+        res.status(200).json({
+            _id:user._id,
+            fullname:user.fullname,
+            username:user.username,
+            email:user.email,
+            followers:user.followers,
+            following:user.following,
+            profileImg:user.profileImg,
+            coverImg:user.coverImg,
+        });
+    }
+    catch(error){
+        console.log("Error is login controller", error.message);
+        res.status(500).json({error: "Internal server error"});
+    }
 }
 
 export const logout = async (req,res) => {
-    res.json({
-        data: "You hit the logout endpoint",
-    });
+    try{
+        res.cookie("jwt","",{maxAge:0})
+        res.status(200).json({message:"Logout Successfully"})
+    }catch(error){
+        console.log("Error is logout controller", error.message);
+        res.status(500).json({error: "Internal server error"});
+    }
 }
+
+export const getMe = async(req,res) => {
+    try{
+        const user = await User.findById(req.user._id).select("-password");
+        res.status(200).json(user);
+    }catch(error){
+        console.log("Error is getMe controller", error.message);
+        res.status(500).json({error: "Internal server error"});
+    }
+ }
+    
