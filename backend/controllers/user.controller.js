43acbcbc336a9b6a47f5643b39ef18dbf859890e
Dulmini Senewirathna <senewirathnapabodha@gmail.com>
2024-07-04 12:51:13 +0530

    commit

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,3 +1,4 @@
+import Notification from "../models/notifications.model.js";
 import User from "../models/user.model.js";
 
 export const getUserProfile = async (req,res) => {
@@ -23,7 +24,7 @@ export const followUnfollowUser = async (req,res) => {
         const currentUser = await User.findById(req.user._id);
 
         if(id === req.user._id.toString()){
-            return res.status(400).json({message: "You can't follow yourself."});
+            return res.status(400).json({error: "You can't follow yourself."});
         }
 
         if(!userToModify || !currentUser) return res.status(400).json({error:"User not found"});
@@ -37,10 +38,43 @@ export const followUnfollowUser = async (req,res) => {
         }else{
             await User.findByIdAndUpdate(id, {$push:{followers: req.user._id}});
             await User.findByIdAndUpdate(req.user._id, {$push: {following : id}});
+
+            const newNotification = new Notification({
+                type : "follow",
+                from : req.user._id,
+                to: userToModify._id,
+            });
+            await newNotification.save();
+
             res.status(200).json({message: "User followed Succesfully"});
         }
     }catch(error){
         console.log("Error in followUnfollowUser: ", error.message);
+        res.status(500).json({error: error.message});
+    }
+}
+
+export const getSuggestedUser = async (req,res) => {
+    try{
+        const userId = req.user._id;
+        const usersFollowedByMe = await User.findById(userId).select("following");
+        const users = await User.aggregate([
+            {
+                $match: {
+                    _id : {$ne: userId},
+                },
+            },
+            { $sample: { size: 10}},
+        ]);
+
+        const filterdUsers = users.filter((user) => !usersFollowedByMe.following.includes(user._id));
+        const suggestedUsers = filterdUsers.slice(0,4);
+
+        suggestedUsers.forEach((user) => (user.password = null));
+        res.status(200).json(suggestedUsers);
+
+    }catch(error){
+        console.log("Error in getSuggestedUser: ", error.message);
         res.status(500).json({error: error.message});
     }
 }
