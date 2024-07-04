0973bfb08aaebb44f83c4b6e7ba1497395c61ead
Dulmini Senewirathna <senewirathnapabodha@gmail.com>
2024-07-04 23:48:25 +0530

    commit

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { protectRoute } from "../middleware/protectRoute.js";
-import { followUnfollowUser, getSuggestedUser, getUserProfile } from "../controllers/user.controller.js";
+import { followUnfollowUser, getSuggestedUser, getUserProfile, updateUser } from "../controllers/user.controller.js";
 
 
 const router = express.Router();
@@ -8,6 +8,6 @@ const router = express.Router();
 router.get("/profile/:username",protectRoute,getUserProfile)
 router.get("/suggested",protectRoute,getSuggestedUser)
 router.post("/follow/:id",protectRoute,followUnfollowUser)
-// router.post("/update",protectRoute,updateUserProfile)
+router.post("/update",protectRoute,updateUser)
 
 export default router
