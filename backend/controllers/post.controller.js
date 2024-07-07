d4b18f2ee5238bf00d4482fc507836534a84df8e
Dulmini Senewirathna <senewirathnapabodha@gmail.com>
2024-07-07 09:01:27 +0530

    commit

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -168,3 +168,56 @@ export const getLikedPosts = async (req,res) => {
         console.log("Error in getLikedPosts controller",error);
     }
 }
+
+export const getFollowingPosts = async (req,res) => {
+    try{
+        const userId = req.user.id;
+        console.log(`Received userId: ${userId}`);
+
+        const user = await User.findById(userId);
+        if(!user) return res.status(404).json({error: "User not found"});
+
+        const following = user.following;
+
+        const feedPosts = await Post.find({user: {$in : following}})
+        .sort({ createdAt : -1})
+        .populate({
+            path: "user",
+            select: "-password",
+        })
+        .populate({
+            path: "comments.user",
+            select: "-password,-email",
+        })
+
+        res.status(200).json(feedPosts);
+
+    }catch(error){
+        res.status(500).json({error: "Internal Server Error"});
+        console.log("Error in getFollowingPosts controller",error);
+    }
+}
+
+export const getUserPosts = async (req,res) => {
+    try{
+        const {username} = req.params;
+
+        const user = await User.findOne({username});
+        if(!user) return res.status(404).json({error: "User not found"});
+
+        const posts = await Post.find({user: user._id}).sort({createdAt: -1}).populate({
+            path: "user",
+            select: "-password",
+        })
+        .populate({
+            path: "comments.user",
+            select: "-password,-email",
+        });
+
+        res.status(200).json(posts);
+
+    }catch(error){
+        res.status(500).json({error: "Internal Server Error"});
+        console.log("Error in getUserPosts controller",error);
+    }
+}
--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,11 +1,13 @@
 import express from "express";
 import { protectRoute } from "../middleware/protectRoute.js";
-import { commentOnPost, createPost, deletePost, getLikedPosts, gettAllPosts, likeUnlikePost } from "../controllers/post.controller.js";
+import { commentOnPost, createPost, deletePost, getFollowingPosts, getLikedPosts, gettAllPosts, getUserPosts, likeUnlikePost } from "../controllers/post.controller.js";
 
 const router = express.Router();
 
 router.get("/all",protectRoute,gettAllPosts);
+router.get("/following",protectRoute,getFollowingPosts);
 router.get("/likes/:id",protectRoute,getLikedPosts)
+router.get("/user/:username", protectRoute,getUserPosts);
 router.post("/create",protectRoute,createPost);
 router.post("/like/:id",protectRoute,likeUnlikePost);
 router.post("/comment/:id",protectRoute,commentOnPost);
